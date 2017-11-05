b8fcbdf51bfdc861f3e988a991b5a01bc05d8e87
nguyenchien97 <chienguyen97@gmail.com>
2017-11-05 21:49:21 +0700

    Add module login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,5 +1,19 @@
 class SessionsController < ApplicationController
+  before_action :user_must_not_login, only: [:new, :create]
   layout "session"
+
   def new
+  end
+
+  def create
+    user = User.find_by(email: params[:session][:email].downcase)
+    if user.present? &&  user.authenticate(params[:session][:password])
+      log_in user
+      flash[:success] = "Đăng nhập thành công"
+      redirect_to root_path
+    else
+      flash[:danger] = "Email hoặc mật khẩu không chính xác"
+      redirect_to login_path
+    end
   end
 end
--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,2 +1,35 @@
 module SessionsHelper
+  def log_in user
+    session[:user_id] = user.id
+  end
+
+  def log_out
+    session.delete(:user_id)
+    @current_user = nil
+  end
+
+  def current_user
+    if session[:user_id]
+      user = User.find_by(id: session[:user_id])
+      @current_user = user
+    end
+  end
+
+  def logged_in?
+    current_user.present?
+  end
+
+  def user_must_logged_in
+    unless logged_in?
+      flash[:warning] = "Bạn cần đăng nhập để tiếp tục"
+      redirect_to login_path
+    end
+  end
+
+  def user_must_not_login
+    if logged_in?
+      flash[:warning] = "Bạn đã đăng nhập. Vui lòng đăng xuất để tiếp tục"
+      redirect_to home_path
+    end
+  end
 end
