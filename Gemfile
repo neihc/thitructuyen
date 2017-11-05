dbe0371d787180b6785f1b615cf39b22bda75297
nguyenchien97 <chienguyen97@gmail.com>
2017-11-06 01:05:41 +0700

    Add admin categories

--- Gemfile
+++ Gemfile
@@ -40,6 +40,7 @@ gem "haml"
 gem "haml-rails"
 gem "config"
 gem "bcrypt"
+gem 'will_paginate', '~> 3.1.0'
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
--- app/controllers/admins/sessions_controller.rb
+++ app/controllers/admins/sessions_controller.rb
@@ -1,5 +1,6 @@
 class Admins::SessionsController < ApplicationController
   before_action :admin_must_not_login, only: [:new, :create]
+  before_action :admin_must_logged_in, only: [:destroy]
   layout "session"
 
   def new
@@ -9,11 +10,17 @@ class Admins::SessionsController < ApplicationController
     admin = Admin.find_by(email: params[:session][:email].downcase)
     if admin.present? &&  admin.authenticate(params[:session][:password])
       admin_log_in admin
-      flash[:success] = "Đăng nhập thành công"
+      flash[:success] = "Login successfully!"
       redirect_to admin_path
     else
-      flash[:danger] = "Email hoặc mật khẩu không chính xác"
+      flash[:danger] = "Email or password is not correct!"
       redirect_to admin_login_path
     end
+  end
+
+  def destroy
+    admin_log_out
+    flash[:success] = "Logout successfully!"
+    redirect_to admin_login_path
   end
 end
